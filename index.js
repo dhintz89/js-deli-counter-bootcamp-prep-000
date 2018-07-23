780ceb686f5b63fbb815722d6513166842cf7322
Dan Hintz <dhintz@live.com>
2018-07-23 21:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ function takeANumber(katzDeliLine,name) {
 function nowServing() {
   if(katzDeli.length >= 1) {
     katzDeli.shift();
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine){
+  return 
 }
