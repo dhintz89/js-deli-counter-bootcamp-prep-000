a50d1f76f866b64575251c6847f0319c44e72320
Dan Hintz <dhintz@live.com>
2018-07-23 23:15:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length >= 1) {
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }else{
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 }
 
 function currentLine(katzDeliLine) {
     if(katzDeliLine.length >= 1) {
-        return 'The line is currently: '
+        return 'The line is currently: ';
         for(var i=0; i<katzDeliLine.length; i++) {
             return `${i+1}. ${katzDeliLine[i]}`;
         }
