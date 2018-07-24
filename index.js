3041027d1ca1a051f939007ea0a8eac85d413b53
Dan Hintz <dhintz@live.com>
2018-07-24 00:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
         for(var i=0; i<katzDeliLine.length; i++) {
             whosInLine.push(`${i+1}. ${katzDeliLine[i]}`);
         }
-        return `The line is currently: ${whosInLine.join(',')
+        return `The line is currently: ${whosInLine.join(', ')
     }else{
         return 'The line is currently empty.';
     }
